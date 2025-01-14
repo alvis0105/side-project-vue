f8b0eb485fbb00d4c1338558ede51daeb13b88aa
alviswu <alviswu@ch-si.com.tw>
2025-01-14 17:18:28 +0800

    修正element-plus全局載入方式及刪除localStorage用法

--- src/main.js
+++ src/main.js
@@ -1,18 +1,13 @@
 import { createApp } from 'vue'
 import { setupStore } from './stores'
 import { setupRouter } from './router'
-import ElementPlus from 'element-plus'
 import App from './App.vue'
 import Plugins from './plugins'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
 setupStore(app)
 setupRouter(app)
 app.use(Plugins)
-app.use(ElementPlus, {
-  locale: zhCn, // 傳入語系物件
-})
 
 app.mount('#app')
